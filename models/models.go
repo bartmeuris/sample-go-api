ab474028d1de880c2f10265b9e1205dc03ea502e
Bart Meuris <b.meuris@napoleongames.be>
2020-01-29 17:30:17 +0100

    Fix typo

--- models/models.go
+++ models/models.go
@@ -7,7 +7,7 @@ func Migrate(db *gorm.DB) error {
 	if err := db.AutoMigrate(&TeamDb{}); err != nil && err.Error != nil {
 		return err.Error
 	}
-	if err := db.AutoMigrate(&git {}); err != nil && err.Error != nil {
+	if err := db.AutoMigrate(&ProjectDb{}); err != nil && err.Error != nil {
 		return err.Error
 	}
 	if err := db.AutoMigrate(&MicroserviceDb{}); err != nil && err.Error != nil {
